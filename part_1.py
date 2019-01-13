9693f670c6a1d3a4c6282ff4e013bceeafde84d4
Jorge Alexander <jorge@maiv.co.uk>
2019-01-14 00:16:07 +0100

    extended model testing

--- part_1.py
+++ part_1.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import pickle
 import os
+from sklearn.model_selection import StratifiedKFold
 from datetime import datetime
 from gensim.models import Word2Vec
 from keras.preprocessing.sequence import pad_sequences
@@ -11,17 +12,29 @@ from keras.models import Sequential
 from keras.callbacks import EarlyStopping, TensorBoard
 from keras.optimizers import Adam
 from keras.layers.normalization import BatchNormalization
-from keras.layers import Dense, LSTM, Embedding, Dropout, SpatialDropout1D, Bidirectional, Conv1D, MaxPooling1D
+from keras.layers import Dense, LSTM, Embedding, SpatialDropout1D, Bidirectional, Conv1D, MaxPooling1D
 from nltk.stem import SnowballStemmer
 from nltk.corpus import stopwords
 from sklearn.metrics import classification_report
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 
+# RUN CONFIG
 TRAINING_DATA_DIR = './datasets/BioASQ-trainingDataset6b.json'
 QUORA_DATA_DIR = './datasets/quora_duplicate_questions.tsv'
 STEM = False
-ALLOWED_STOPWORDS = ['does', 'what', 'why', 'how', 'which', 'where', 'when', 'who']
+Q_CLASSES = ['factoid', 'list', 'summary', 'yesno']
+# ALLOWED_STOPWORDS = ['does', 'what', 'why', 'how', 'which', 'where', 'when', 'who', 'is', 'are']
+TEST_SPLIT = 0.1
+VALIDATION_SPLIT = 0.1
+TRAINABLE_EMBEDDING = False
+USE_CONV = True
+CROSS_VALIDATION = False
+LEARNING_RATE = 0.001
+BATCH_SIZE = 128
+DROPOUT = 0.3
+EPOCHS = 100
+USE_W2V_EMBED = True
 WORD2VEC_PARAMS = {
     'size': 100,
     'min_count': 5,
@@ -30,22 +43,15 @@ WORD2VEC_PARAMS = {
     'window': 10,
     'workers': 16
 }
-TEST_SPLIT = 0.1
-VALIDATION_SPLIT = 0.1
-USE_W2V_EMBED = True
-TRAINABLE_EMBEDDING = False
-USE_CONV = False
-LEARNING_RATE = 0.001
-BATCH_SIZE = 128
-DROPOUT = 0.3
-EPOCHS = 100
+# END RUN CONFIG
+
 # Logging
 LOG_LEVEL = 1
-LOG_DIR = "./Graph/" + str(datetime.utcnow())
+LOG_DIR = "./Graphs_datetime/" + str(datetime.utcnow())
 
 
-def parse_questions(data):
-    return zip(*[[d['body'], d['type']] for d in data['questions']])
+def get_test_split():
+    return TEST_SPLIT
 
 
 def get_q_texts_tokens():
@@ -53,6 +59,10 @@ def get_q_texts_tokens():
     [q_texts, q_labels] = parse_questions(json_to_df(TRAINING_DATA_DIR))
     q_texts_tokens = [text_to_tokens(text) for text in q_texts]
     return q_texts_tokens, q_labels
+
+
+def parse_questions(data):
+    return zip(*[[d['body'], d['type']] for d in data['questions']])
 
 
 def label_to_class(str_labels, label):
@@ -69,7 +79,7 @@ def text_to_tokens(text):
     text = text.lower()
     text = re.sub(r"\?", " QUESTION ", text)
     text = re.sub("\d+", " DIGIT ", text)
-    text = re.sub(r"\'s", "  ", text)
+    text = re.sub(r"\'s", " is ", text)
     text = re.sub(r"\'ve", " have ", text)
     text = re.sub(r"n't", " not ", text)
     text = re.sub(r"i'm", " i am ", text)
@@ -77,9 +87,9 @@ def text_to_tokens(text):
     text = re.sub(r"\'d", " would ", text)
     text = re.sub(r"\'ll", " will ", text)
     text = re.sub(r"[^\w\s]", " ", text)
-
     tokens = []
-    ignore_words = set(stopwords.words("english")) - set(ALLOWED_STOPWORDS)
+    ignore_words = []
+    # (set(stopwords.words("english")) - set(ALLOWED_STOPWORDS))
 
     for word in text.split():
         if word not in ignore_words:
@@ -104,27 +114,31 @@ def build_vocab(tokens_list_list):
     return vocab
 
 
-def one_hot_encode_labels(q_labels, q_classes):
+def one_hot_encode_labels(q_labels):
+    q_classes = get_q_classes()
     y = []
 
     for q_label in q_labels:
         enc = np.zeros(len(q_classes))
         enc[q_classes.index(q_label)] = 1
         y.append(enc)
+
     return np.array(y)
 
 
-def build_model(tokens, vocab, max_seq_length, output_size, embed_w2v, dropout, learning_rate):
-    vocab_size = len(vocab.keys())
-    vector_model = Word2Vec(tokens, **WORD2VEC_PARAMS)
+def build_model(vocab, max_seq_length, output_size, embed_w2v, dropout, learning_rate):
+    vocab_tokens = vocab.keys()
+    vocab_size = len(vocab_tokens)
+    vector_model = Word2Vec(vocab_tokens, **WORD2VEC_PARAMS)
     embedding_weights = (np.random.rand(vocab_size, WORD2VEC_PARAMS['size']) - 0.5) / 5.0
 
     if embed_w2v:
-        for word, idx in vocab.items():
+        for token in vocab_tokens:
             try:
-                embedding_weights[idx] = vector_model.wv[word]
+                idx = vocab[token]
+                embedding_weights[idx] = vector_model.wv[token]
             except KeyError:
-                logger('KeyError' + word, 0)
+                logger('KeyError' + token, 0)
                 pass
 
     embedding = embedding_layer(input_sequence_size=max_seq_length,
@@ -154,7 +168,7 @@ def embedding_layer(vocab_size, input_sequence_size, embedding_weights):
     embedding_arguments = {
         'input_dim': vocab_size,
         'input_length': input_sequence_size,
-        'mask_zero': True,  # index 0 reserved for padding
+        'mask_zero': False,  # index 0 reserved for padding
         'output_dim': WORD2VEC_PARAMS['size'],
         'trainable': TRAINABLE_EMBEDDING,
         'weights': [embedding_weights]
@@ -181,30 +195,27 @@ def plot_history(history):
     plt.show()
 
 
-def save_run_config():
-    run_config = {
+def get_run_config():
+    return {
         'STEM': STEM,
-        'ALLOWED_STOPWORDS': ALLOWED_STOPWORDS,
+        'BATCH_SIZE': BATCH_SIZE,
+        'DROPOUT': DROPOUT,
+        'EPOCHS': EPOCHS,
+        'LEARNING_RATE': LEARNING_RATE,
         'WORD2VEC_PARAMS': WORD2VEC_PARAMS,
         'TEST_SPLIT': TEST_SPLIT,
         'VALIDATION_SPLIT': VALIDATION_SPLIT,
         'USE_W2V_EMBED': USE_W2V_EMBED,
         'TRAINABLE_EMBEDDING': TRAINABLE_EMBEDDING,
         'USE_CONV': USE_CONV,
-        'LEARNING_RATE': LEARNING_RATE,
-        'BATCH_SIZE': BATCH_SIZE,
-        'DROPOUT': DROPOUT,
-        'EPOCHS': EPOCHS
+        'CROSS_VALIDATION': CROSS_VALIDATION
     }
 
-    if not os.path.exists(LOG_DIR):
-        os.makedirs(LOG_DIR)
 
-    with open(os.path.join(LOG_DIR, 'run_config.json'), 'w') as fp:
-        json.dump(run_config, fp)
+def save_vocab_model_report(log_dir, vocab, model, report, run_config):
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
 
-
-def save_vocab_model_report(log_dir, vocab, model, report):
     with open(os.path.join(log_dir, 'vocab.pickle'), 'wb') as fp:
         pickle.dump(vocab, fp)
 
@@ -214,8 +225,11 @@ def save_vocab_model_report(log_dir, vocab, model, report):
     with open(os.path.join(log_dir, 'classification_report.pickle'), 'wb') as fp:
         pickle.dump(report, fp)
 
+    with open(os.path.join(log_dir, 'run_config.json'), 'w') as fp:
+        json.dump(run_config, fp)
 
-def get_quora_duplicate_tokens(limit):
+
+def get_quora_duplicate_tokens(limit=False):
     df = pd.DataFrame.from_csv(QUORA_DATA_DIR, sep='\t')
     duplicates = df[df['is_duplicate'] == 1]
 
@@ -235,73 +249,86 @@ def get_max_seq_length(model):
     return model.layers[0].get_output_at(0).get_shape().as_list()[1]
 
 
-def get_q_classes(q_labels):
-    return list(np.unique(q_labels))
+def get_q_classes():
+    return Q_CLASSES
 
 
 def tokens_to_sequences(vocab_idx_dict, texts_tokens):
     return [[vocab_idx_dict[token] for token in tokens] for tokens in texts_tokens]
 
 
-def train_test_model(model, sequences, q_labels, epochs, batch_size, test_idx=False):
-    # build data x and target y
-    x = pad_sequences(sequences, maxlen=get_max_seq_length(model), padding="pre", truncating="post")
-    y = one_hot_encode_labels(q_labels, get_q_classes(q_labels))
-
-    # Train test split
-    if type(test_idx) == int:
-        x_test, y_test = x[:test_idx], y[:test_idx]
-        x_train, y_train = x[test_idx:], y[test_idx:]
-    else:
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=TEST_SPLIT)
-
-    # Keras callbacks
+def train_test_model(model, x_train, x_test, y_train, y_test, epochs, batch_size, log_dir):
     callbacks = [
         EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=0, mode='auto'),
-        TensorBoard(log_dir=LOG_DIR, histogram_freq=0, write_graph=True, write_images=True)
+        TensorBoard(log_dir=log_dir, histogram_freq=0, write_graph=True, write_images=True)
     ]
-
     # Train the model
-    hist = model.fit(x_train, y_train, callbacks=callbacks, validation_split=VALIDATION_SPLIT, epochs=epochs,
-                     batch_size=batch_size, shuffle=True)
-
+    hist = model.fit(x_train, y_train, callbacks=callbacks, validation_split=VALIDATION_SPLIT, epochs=epochs, batch_size=batch_size, shuffle=True)
     # Test the model
     plot_history(pd.DataFrame(hist.history))
     y_hat = model.predict(x_test)
-    report = classification_report(np.argmax(y_test, axis=1), np.argmax(y_hat, axis=1), target_names=get_q_classes(q_labels))
+    report = classification_report(np.argmax(y_test, axis=1), np.argmax(y_hat, axis=1), target_names=get_q_classes(), output_dict=False)
     logger(report, 1)
     return model, report
 
 
-def run():
-    save_run_config()
-
+def run(quora_tokens=False):
     # Original tokens to classify
     q_texts_tokens, q_labels = get_q_texts_tokens()
-
     # Additional tokens to use later
-    quora_tokens_q_1, quora_tokens_q_2 = get_quora_duplicate_tokens(limit=False)
-    quora_tokens = quora_tokens_q_1 + quora_tokens_q_2
-
+    if not quora_tokens:
+        quora_tokens_q_1, quora_tokens_q_2 = get_quora_duplicate_tokens(limit=False)
+        quora_tokens = quora_tokens_q_1 + quora_tokens_q_2
     # Embedding creation
-    vocab = build_vocab(q_texts_tokens + quora_tokens)
+    all_tokens = q_texts_tokens + quora_tokens
+    vocab = build_vocab(all_tokens)
     sequences = tokens_to_sequences(vocab, q_texts_tokens)
-    max_sequence_length = max([len(tokens) for tokens in q_texts_tokens + quora_tokens])
+    max_sequence_length = max([len(tokens) for tokens in all_tokens])
+    model = build_model(vocab, max_sequence_length, len(get_q_classes()), USE_W2V_EMBED, DROPOUT, LEARNING_RATE)
+    logger(model.summary())
+    # build x and y
+    x = pad_sequences(sequences, maxlen=get_max_seq_length(model), padding="pre", truncating="post")
+    y = one_hot_encode_labels(q_labels)
+    # Train test split
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=TEST_SPLIT, shuffle=False)
+    model, report = train_test_model(model, x_train, x_test, y_train, y_test, EPOCHS, BATCH_SIZE, LOG_DIR)
 
-    model = build_model(
-        q_texts_tokens,
-        vocab,
-        max_sequence_length,
-        len(get_q_classes(q_labels)),
-        USE_W2V_EMBED,
-        DROPOUT,
-        LEARNING_RATE)
-
-    trained_model, report = train_test_model(model, sequences, q_labels, EPOCHS, BATCH_SIZE, False)
-    save_vocab_model_report(LOG_DIR, vocab, trained_model, report)
-
-    return vocab, trained_model
+    logger(report, 1)
+    save_vocab_model_report(LOG_DIR, vocab, model, report, get_run_config())
+    return vocab, model
 
 
 if __name__ == "__main__":
     run()
+
+# Cross validation code
+# if CROSS_VALIDATION:
+#     k_fold = StratifiedKFold(n_splits=10, shuffle=True)
+#     report = {
+#         'factoid': {'precision': [], 'recall': [], 'f1-score': [], 'support': []},
+#         'list': {'precision': [], 'recall': [], 'f1-score': [], 'support': []},
+#         'summary': {'precision': [], 'recall': [], 'f1-score': [], 'support': []},
+#         'yesno': {'precision': [], 'recall': [], 'f1-score': [], 'support': []}
+#     }
+#     target_names = get_q_classes()
+#     metrics = ['precision', 'recall', 'f1-score', 'support']
+#
+#     for train, test in k_fold.split(x, y):
+#         y_train = y_encoded[train]
+#         y_test = y_encoded[test]
+#         hist = model.fit(x[train], y_train, callbacks=callbacks, validation_split=VALIDATION_SPLIT,
+#                                               epochs=EPOCHS, batch_size=BATCH_SIZE)
+#         plot_history(pd.DataFrame(hist.history))
+#         y_hat = model.predict(x[test])
+#         class_report = classification_report(np.argmax(y_test, axis=1), np.argmax(y_hat, axis=1),
+#                                                       target_names=target_names, output_dict=True)
+#
+#         for target in target_names:
+#             for metric in metrics:
+#                 report[target][metric].append(class_report[target][metric])
+#
+#     for target in target_names:
+#         for metric in metrics:
+#             cv_scores = report[target][metric]
+#             report[target][metric] = "%.2f%% (+/- %.2f%%)" % (np.mean(cv_scores), np.std(cv_scores))
+# else:
--- part_2.py
+++ part_2.py
@@ -3,16 +3,20 @@ import part_1
 import numpy as np
 import os
 from keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
 
-FROM_CACHE = True
-MODEL_DIR = "./Graph/2019-01-07 11:34:17.175065/model.pickle"
-VOCAB_DIR = "./Graph/2019-01-07 11:34:17.175065/vocab.pickle"
-EPOCHS = 100
-BATCH_SIZE = 128
-SEQUENCE_LIMIT = 50000
-LEARNING_RATE = 0.001
-DROPOUT = 0.3
-ALLOWED_CLASS = ['factoid']
+# RUN CONFIG
+RUN_CONFIG = {
+    "FROM_CACHE": False,
+    "MODEL_DIR": "./Graphs_datetime/2019-01-11 17:35:16.028132/model.pickle",
+    "VOCAB_DIR": "./Graphs_datetime/2019-01-11 17:35:16.028132/vocab.pickle",
+    "EPOCHS": 50,
+    "BATCH_SIZE": 512,
+    "EXTRA_TOKENS": 50000,
+    "LEARNING_RATE": 0.001,
+    "DROPOUT": 0.5,
+    "ALLOWED_CLASS": 'factoid'
+}
 
 
 def load_pickle(file_path):
@@ -20,82 +24,109 @@ def load_pickle(file_path):
         return pickle.load(f)
 
 
-def get_max_seq_length(model):
-    return model.layers[0].get_output_at(0).get_shape().as_list()[1]
+def save_pickle(obj, file_path):
+    with open(file_path, 'wb') as fp:
+        pickle.dump(obj, fp)
 
 
-def build_new_tokens(question_1, question_1_predictions, question_2, question_2_predictions, q_classes):
-    print("Building new tokens")
+def predict_labels(vocab, model, q_tokens_list, max_seq_length, class_label_map):
+    sequence = pad_sequences([[vocab[token] for token in q_tokens] for q_tokens in q_tokens_list], maxlen=max_seq_length, padding="pre", truncating="post")
+    classes = np.argmax(model.predict(sequence), axis=1)
+    return [class_label_map[_class] for _class in classes]
+
+
+def build_new_tokens(q_1_tokens_list, q_2_tokens_list, q_1_pred_labels, q_2_pred_labels, allowed_class, token_limit):
     new_tokens = []
     new_labels = []
+    limit = 0
 
-    for idx, prediction_1 in enumerate(question_1_predictions):
-        prediction_2 = question_2_predictions[idx]
-        _class = q_classes[prediction_2]
+    for q_1_tokens, q_2_tokens, q_1_pred_label, q_2_pred_label in zip(q_1_tokens_list, q_2_tokens_list, q_1_pred_labels, q_2_pred_labels):
 
-        if prediction_1 == prediction_2 and _class in ALLOWED_CLASS:
-            new_tokens.append(question_1[idx])
-            new_labels.append(_class)
-            new_tokens.append(question_2[idx])
-            new_labels.append(_class)
+        if limit >= token_limit:
+            break
+
+        if allowed_class and q_1_pred_label not in allowed_class:
+            continue
+
+        if q_1_pred_label == q_2_pred_label:
+            new_tokens.append(q_1_tokens)
+            new_labels.append(q_1_pred_label)
+            new_tokens.append(q_2_tokens)
+            new_labels.append(q_2_pred_label)
+            limit += 2
 
     return new_tokens, new_labels
 
 
+def token_combine_split(vocab, old_tokens, old_labels, new_tokens, new_labels, max_seq_len):
+    train_tokens, test_tokens, train_labels, test_labels = train_test_split(old_tokens, old_labels, test_size=part_1.get_test_split(), shuffle=False)
+    train_tokens = train_tokens + list(new_tokens)
+    train_labels = train_labels + list(new_labels)
+    train_sequences = pad_sequences(part_1.tokens_to_sequences(vocab, train_tokens), maxlen=max_seq_len, padding="pre", truncating="post")
+    test_sequences = pad_sequences(part_1.tokens_to_sequences(vocab, test_tokens), maxlen=max_seq_len, padding="pre", truncating="post")
+
+    return train_sequences, test_sequences, part_1.one_hot_encode_labels(train_labels), part_1.one_hot_encode_labels(test_labels)
+
+
 def run():
-    if FROM_CACHE and MODEL_DIR and VOCAB_DIR:
-        model = load_pickle(MODEL_DIR)
-        vocab = load_pickle(VOCAB_DIR)
+    tokens_cache_name = 'quora_tokens.pickle'
+    if RUN_CONFIG['FROM_CACHE'] and os.path.exists(tokens_cache_name):
+        print("LOADING QUORA DUPLICATES AS TOKENS FROM CACHE")
+        tokens_question_1, tokens_question_2 = load_pickle(tokens_cache_name)
     else:
-        vocab, model = part_1.run()
+        print("PARSING QUORA DUPLICATES AS TOKENS")
+        tokens_question_1, tokens_question_2 = part_1.get_quora_duplicate_tokens()
+        save_pickle([tokens_question_1, tokens_question_2], tokens_cache_name)
 
-    print("LOADING QUORA DUPLICATES")
-    question_1, question_2 = part_1.get_quora_duplicate_tokens(SEQUENCE_LIMIT)
-    max_seq_length = get_max_seq_length(model)
-    question_1_x = pad_sequences(part_1.tokens_to_sequences(vocab, question_1),
-                                 maxlen=max_seq_length, padding="pre", truncating="post")
-    question_2_x = pad_sequences(part_1.tokens_to_sequences(vocab, question_2),
-                                 maxlen=max_seq_length, padding="pre", truncating="post")
+    if RUN_CONFIG['FROM_CACHE'] and RUN_CONFIG['MODEL_DIR'] and RUN_CONFIG['VOCAB_DIR']:
+        print("LOADING ORIGINAL MODEL FROM CACHE")
+        original_model = load_pickle(RUN_CONFIG['MODEL_DIR'])
+        vocab = load_pickle(RUN_CONFIG['VOCAB_DIR'])
+    else:
+        print("BUILDING ORIGINAL MODEL")
+        vocab, original_model = part_1.run(tokens_question_1 + tokens_question_2)
 
-    print("MAKING PREDICTIONS")
-    question_1_predictions = np.argmax(model.predict(question_1_x), axis=1)
-    question_2_predictions = np.argmax(model.predict(question_2_x), axis=1)
+    print("LOADING ORIGINAL TOKENS")
+    max_seq_length = part_1.get_max_seq_length(original_model)
+    original_classes = part_1.get_q_classes()
+    original_tokens, original_labels = part_1.get_q_texts_tokens()
 
-    print("BUILDING NEW MODEL")
-    old_tokens, old_labels = part_1.get_q_texts_tokens()
-    q_classes = part_1.get_q_classes(old_labels)
+    predict_cache_name = 'predictions.pickle'
+    if RUN_CONFIG['FROM_CACHE'] and os.path.exists(predict_cache_name):
+        print("LOADING QUORA DUPLICATE PREDICTIONS")
+        q_1_predictions, q_2_predictions = load_pickle('predictions.pickle')
+    else:
+        print("PREDICTING QUORA DUPLICATES")
+        q_1_predictions = predict_labels(vocab, original_model, tokens_question_1, max_seq_length, original_classes)
+        q_2_predictions = predict_labels(vocab, original_model, tokens_question_2, max_seq_length, original_classes)
+        save_pickle([q_1_predictions, q_2_predictions], predict_cache_name)
 
-    new_tokens, new_labels = build_new_tokens(
-        question_1, question_1_predictions,
-        question_2, question_2_predictions,
-        q_classes)
+    for _class in ['factoid', 'summary']:  #
+        RUN_CONFIG['ALLOWED_CLASS'] = _class
 
+        for limit in [22000]:  #1000, 6000, 11000,
+            RUN_CONFIG['LEARNING_RATE'] = limit
+            print("BUILDING NEW TOKENS")
+            new_tokens, new_labels = build_new_tokens(tokens_question_1, tokens_question_2, q_1_predictions, q_2_predictions, allowed_class=_class, token_limit=limit)
+            x_train, x_test, y_train, y_test = token_combine_split(vocab, original_tokens, original_labels, new_tokens, new_labels, max_seq_length)
+            print("TRAINING NEW MODEL")
 
-    new_tokens = old_tokens + new_tokens
-    new_labels = list(old_labels) + list(new_labels)
-    new_model = part_1.build_model(
-        new_tokens,
-        vocab,
-        max_seq_length,
-        len(q_classes),
-        True,
-        DROPOUT,
-        LEARNING_RATE
-    )
-    new_sequences = part_1.tokens_to_sequences(vocab, new_tokens)
+            for rate in [0.0001, 0.001, 0.01]:
+                RUN_CONFIG['LEARNING_RATE'] = rate
 
-    print("TRAINING NEW MODEL")
+                for dropout in [0.3, 0.5, 0.7]:
+                    RUN_CONFIG['DROPOUT'] = dropout
 
-    new_trained_model, new_report = part_1.train_test_model(
-        new_model,
-        new_sequences,
-        new_labels,
-        EPOCHS,
-        BATCH_SIZE,
-        test_idx=int(0.1*len(old_tokens))
-    )
+                    for batch_size in [1024]: #125, 512,
+                        RUN_CONFIG['BATCH_SIZE'] = batch_size
+                        log_dir = "./Graph_3/" + _class + "_" + str(limit) + "_" + str(rate) + "_" + str(dropout) + "_" + str(batch_size)
+                        new_model = part_1.build_model(vocab, max_seq_length, len(original_classes), embed_w2v=True, dropout=dropout, learning_rate=rate)
+                        new_trained_model, new_report = part_1.train_test_model(new_model, x_train, x_test, y_train, y_test,
+                                                                                epochs=RUN_CONFIG['EPOCHS'],
+                                                                                batch_size=RUN_CONFIG['BATCH_SIZE'],
+                                                                                log_dir=log_dir)
 
-    part_1.save_vocab_model_report('.', vocab, new_trained_model, new_report)
+                        part_1.save_vocab_model_report(log_dir, vocab, new_trained_model, new_report, RUN_CONFIG)
 
 
 if __name__ == "__main__":
